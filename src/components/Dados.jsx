6c8e47043e2c6a87764b72b6908e241039f6d9a3
winvirg <thiagowinvirg@gmail.com>
2021-09-28 19:20:10 -0300

    PrimeiroPrimeiro Codigo Completo

---
+++ src/components/Dados.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Dados(){
+    return (
+        <div>
+            <h3>Nome:</h3>
+            Thiago Winvirg Nobre Bezerra
+            <h3>Idade:</h3>
+            25 anos
+            <h3>Curso:</h3>
+            Ciencia da computacao
+            <h3>Cidade:</h3>
+            Morada Nova-CE
+            <h3>Email cadastrado:</h3>
+            thiagowinvirg@alu.ufc.br
+        </div>
+    )
+}
+    export default Dados
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import reactDOM from "react-dom";
+
+import Dados from "./components/Dados";
+
+const root = document.getElementById('root');
+reactDOM.render(<Dados/>,root);
